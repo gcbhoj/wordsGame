0f3f95b5c71499d69a2affbee1821ef0dcd75a79
BhojGC <ghartich@sheridancollege.ca>
2025-05-02 01:38:08 -0400

    inital data read

--- script/script.js
+++ script/script.js
@@ -1,4 +1,19 @@
 window.addEventListener("load", () => {
-  
-  console.log("Page fully loaded!");
+  let completeData = [];
+
+  fetch(
+    "https://raw.githubusercontent.com/gcbhoj/SBDatabase/main/DataBase.json"
+  )
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log("JSON DATA", jsonData);
+
+      if (jsonData != null) {
+        completeData.push(...jsonData);
+
+        for (let i = 0; i < completeData.length; i++) {
+          console.log("Complete Data", completeData[i].category);
+        }
+      }
+    });
 });
