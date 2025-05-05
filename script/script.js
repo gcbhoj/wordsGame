a42ef5fd2a0bec884bbd427869fd0b2d10b648c7
BhojGC <ghartich@sheridancollege.ca>
2025-05-04 20:20:34 -0400

    third page

--- script/script.js
+++ script/script.js
@@ -3,20 +3,42 @@ import { loadVowels, loadConsonents, loadData } from "./loadData.js";
 const completeVowels = [];
 const completeConsonents = [];
 const completeData = [];
+const threePage = document.getElementById("three_four_page");
+const pageTitle = document.getElementById("page-head");
 
 window.addEventListener("load", () => {
+  const vowelPagesContainer = document.querySelector(".pages");
+  let pageNumber = 1;
   //loading all vowels from database
   loadVowels().then((vowels) => {
     completeVowels.push(...vowels);
     // console.log("Loaded Vowels:", completeVowels);
 
-    const 
     completeVowels.forEach((item) => {
       //  console.log("Vowels", item.alphabet);
+      const page = document.createElement("div");
+      page.classList.add("page");
 
+      //creating front side
+      const front = document.createElement("div");
+      front.classList.add("page-side", "front");
+
+      front.innerHTML = `
+<div class="page-head">
+          <div class="page-title">${item.alphabet}</div>
+        </div>
+        <div class="page-content">
+          <img src="${item.vowelsImage}" alt="${item.alphabet}" width="100">
+        </div>
+        <div class="page-footer">
+          <span>May 2025</span>
+          <span class="pg-no">${pageNumber}</span>
+        </div>
+      
+      `;
+      pageNumber++;
     });
   });
-  displayAlphabetImage();
 
   //loading all consonets
   loadConsonents().then((consonents) => {
@@ -35,16 +57,3 @@ window.addEventListener("load", () => {
     //console.log("All Data", data);
   });
 });
-
-function displayAlphabetImage() {
-  const vowelsImage = document.getElementById("vowelsImageDisplay");
-  let images = "";
-
-  completeVowels.forEach((element) => {
-    images += `<img src="${element.image}" alt="Vowel" style="width:30px; margin-right:5px;">`;
-  });
-  console.log(images)
-
-  vowelsImage.innerHTML = images;
-}
-
